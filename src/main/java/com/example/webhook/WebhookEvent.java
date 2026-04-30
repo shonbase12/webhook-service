30e3dc9b506016e17c49cfd70dd91440fa82992c
shonbase12 <shonbase12@gmail.com>
2026-04-30 15:12:10 +0300

    Create WebhookEvent class with eventType property

---
+++ src/main/java/com/example/webhook/WebhookEvent.java
@@ -0,0 +1,14 @@
+public class WebhookEvent {
+    private String eventType;
+
+    public WebhookEvent() {
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+}
